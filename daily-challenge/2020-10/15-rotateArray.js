eb57bda03e0e0314242e1528dc962e67eb0ce98b
Adam <gienappa@gmail.com>
2020-10-15 11:38:09 -0700

    added 10-15 "best" solution

--- daily-challenge/2020-10/15-rotateArray.js
+++ daily-challenge/2020-10/15-rotateArray.js
@@ -20,3 +20,21 @@ var rotate = function(nums, k) {
   }
   return nums;
 };
+
+
+// Popular / A+ turbo-nerd solution
+var rotate = function(nums, k) {
+  k = k % nums.length;
+
+  flip(nums, 0, nums.length - 1);
+  flip(nums, 0, k - 1);
+  flip(nums, k, nums.length - 1);
+};
+
+const flip = (arr, start, end) => {
+  while (start < end) {
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+    start++;
+    end--;
+  }
+};
