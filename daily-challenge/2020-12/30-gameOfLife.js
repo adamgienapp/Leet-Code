9160b8cf2e8ad708c6282856802598e6546b8bbd
Adam Gienapp <gienappa@gmail.com>
2020-12-30 13:12:03 -0800

    12-30 revised

--- daily-challenge/2020-12/30-gameOfLife.js
+++ daily-challenge/2020-12/30-gameOfLife.js
@@ -5,50 +5,50 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var gameOfLife = function(board) {
+    const [m, n] = [board.length, board[0].length];
+    
+    const deltas = [
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, -1],
+        [0, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1]
+    ];
+    
     const checkNeighbors = (x, y) => {
         let neighbors = 0;
 
-        const deltaX = [-1, 0, 1];
-        const deltaY = [-1, 0, 1];
+        for (const [dX, dY] of deltas) {
+            let [vX, vY] = [x + dX, y + dY];
 
-        for (const dX of deltaX) {
-            let vX = x + dX;
-            if (vX < 0 || vX === board[0].length) continue;
-
-            for (const dY of deltaY) {
-                let vY = y + dY;
-
-                if (vY < 0 || vY === board.length || (dX === 0 && dY === 0)) continue;
-
-                if (board[vY][vX] === 1 || board[vY][vX] === '-') neighbors++;
-            }
+            if (vX >= 0 && vX < n && vY >= 0 && vY < m && Math.abs(board[vY][vX]) === 1) neighbors++;
         }
 
         return neighbors;
     }
     
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[0].length; j++) {
-            let numNeighbors = checkNeighbors(j, i);
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            let numNeighbors = checkNeighbors(j, i, board);
             let cell = board[i][j];
-            if (cell === 1) {
-                if (numNeighbors < 2 || numNeighbors > 3) {
-                    board[i][j] = '-';
-                }
-            } else {
-                if (numNeighbors === 3) {
-                    board[i][j] = '+';
-                }
+            if (cell === 1 && (numNeighbors < 2 || numNeighbors > 3)) {
+                board[i][j] = -1;
+            }
+            if (cell === 0 && numNeighbors === 3) {
+                board[i][j] = 2;
             }
         }
     }
     
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[0].length; j++) {
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
             let cell = board[i][j];
-            if (cell === '-') {
+            if (cell === -1) {
                 board[i][j] = 0;
-            } else if (cell === '+') {
+            } else if (cell === 2) {
                 board[i][j] = 1;
             }
         }
