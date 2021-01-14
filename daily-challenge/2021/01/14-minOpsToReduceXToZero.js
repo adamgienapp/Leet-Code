418ca56f18d428f920f48aab3ec9142efe37ea73
Adam Gienapp <gienappa@gmail.com>
2021-01-14 11:27:17 -0800

    added 1-14

---
+++ daily-challenge/2021/01/14-minOpsToReduceXToZero.js
@@ -0,0 +1,28 @@
+// PROMPT: https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/
+
+/**
+ * @param {number[]} nums
+ * @param {number} x
+ * @return {number}
+ */
+var minOperations = function(nums, x) {
+  const sum = nums.reduce((a,b) => a + b, 0);
+  const target = sum - x;
+  
+  let [l, r] = [0, 0];
+  let currSum = 0;
+  let maxLen = -Infinity;
+  
+  while (r < nums.length) {
+      currSum += nums[r];
+      while (currSum >= target && l < nums.length) {
+          if (currSum === target) {
+              maxLen = Math.max(maxLen, r - l + 1);
+          }
+          currSum -= nums[l++];
+      }
+      r++;
+  }
+  
+  return maxLen === -Infinity ? -1 : nums.length - maxLen;
+};
